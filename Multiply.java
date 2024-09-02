659feb3a8f15776411708d9f8f0cb09f65892c68
Sanjeev Jain <jain.me@hotmail.com>
2024-09-02 19:26:07 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+        public static void main(String[] args) {
+            int a=5;
+            int b=10;
+    System.out.println("The product is: "= a*b);
+        }
+    
+    }
+}
